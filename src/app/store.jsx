66651ec98946a97faab95445656c623ba7a4f760
simonjordan93 <simon.m.jordan@gmail.com>
2023-04-14 23:51:42 +0200

    tut done

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,13 +1,12 @@
 // Import the `configureStore` function from the `@reduxjs/toolkit` package
 import { configureStore } from "@reduxjs/toolkit";
-
-// Import the `counterReducer` from the `counterSlice` file, which is a slice reducer created using `createSlice` function
-import counterReducer from "../features/counter/counterSlice";
+import postsReducer from "../features/posts/postsSlice";
+import usersReducer from "../features/users/usersSlice";
 
 // Use the `configureStore` function to create a Redux store
 export const store = configureStore({
   reducer: {
-    // An object that specifies how the store's state is updated based on the actions dispatched to the store
-    counter: counterReducer, // Associates the `counter` slice reducer with the `counter` key in the store's state
+    posts: postsReducer,
+    users: usersReducer,
   },
 });
